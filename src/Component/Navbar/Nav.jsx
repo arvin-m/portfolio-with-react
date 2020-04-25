bb1c3cbaadfb9408fbb4be29c2170a759648470b
Arvin Mahmoudi <mahmoudi.arvin@gmail.com>
2020-04-24 22:06:01 -0400

    adding Navbar and AboutMe in to the DOM

---
+++ src/Component/Navbar/Nav.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import '../Navbar/style.css';
+
+function Navbar() {
+
+
+
+
+
+
+    return (
+
+    <nav className="navbar navbar-expand-md navbar-dark bg-dark sticky-top">
+
+
+            <div className="container-fluid">
+                <div>
+                    <h1 className="navbar-brand ">Arvin Mahmoudi</h1>
+                </div>
+                <div>
+                    <p id="header-p"> full stack web developer</p>
+                </div>
+
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive"
+                    aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarResponsive">
+
+                    <ul className="navbar-nav ml-auto">
+                        <li className="nav-item">
+                            <a href="index.html" className="nav-link fas fa-home" data-toggle="tooltip" data-placement="bottom"
+                                title="Home"><span className="label"></span></a>
+                        </li>
+
+                        <li className="nav-item">
+                            <a href="resume.html" className=" nav-link far fa-file-alt" data-toggle="tooltip" data-placement="bottom"
+                                title="Resume" ><span className="label"></span></a>
+                        </li>
+                        <li className="nav-item">
+                            <a href="https://www.linkedin.com/in/arvin-mahmoudi" className=" nav-link  fab fa-linkedin"
+                                data-toggle="tooltip" data-placement="bottom" title="Linkedin" ><span
+                                    className="label"></span></a>
+                        </li>
+                        <li className="nav-item">
+                            <a href="https://github.com/arvin-m" className="nav-link  fab fa-github-square "
+                                data-toggle="tooltip" data-placement="bottom" title="GitHub"><span className="label"></span></a>
+                        </li>
+
+
+
+                    </ul>
+                </div>
+            </div>
+    </nav>
+
+
+    )
+}
+
+
+export default Navbar;
