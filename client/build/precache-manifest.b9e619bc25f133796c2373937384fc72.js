c1b0c4513ce889f882eea40822cc952b78802d45
Arvin Mahmoudi <mahmoudi.arvin@gmail.com>
2020-04-29 16:06:06 -0400

    adding client folder

---
+++ client/build/precache-manifest.b9e619bc25f133796c2373937384fc72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c3d1af1ca02a9be729073f286e45b95",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ac9d8fa5b468f08869d",
+    "url": "/static/css/main.b0cbb9b9.chunk.css"
+  },
+  {
+    "revision": "2bdf68afd6297772ad35",
+    "url": "/static/js/2.fc27ff22.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fc27ff22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ac9d8fa5b468f08869d",
+    "url": "/static/js/main.50602225.chunk.js"
+  },
+  {
+    "revision": "9ec373ce40b630971588",
+    "url": "/static/js/runtime-main.71855e1f.js"
+  }
+]);
